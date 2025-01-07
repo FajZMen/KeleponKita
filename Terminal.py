4336b89f4ca0d3bb5febe2ff08735f236a71bfa3
FajZ <fajarzman52@gmail.com>
2025-01-07 19:20:42 +0700

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -1,18 +1,19 @@
 from time import sleep as wait
 import streamlit as st
 import pandas as pd
-from Functions.Function import sesi_inisilasi, tambah_ke_keranjang, login, katalogfunc, keranjangfunc, historypesanan, downloadlist, historydelete, vouchermaker, voucherdeleter, adminchat, accountbank, accountcreatortool, accountdeletortool, customersupport, deletesupportreport
-from Data.Datas import produk, accounts, adminaccounts, superadminaccounts, historypesananlist, vouchers, adminchathistory, supporthistory
-sesi_inisilasi()
+from Functions.Function import getstates, tambah_ke_keranjang, login, register, katalogfunc, keranjangfunc, historypesanan, downloadlist, historydelete, vouchermaker, voucherdeleter, adminchat, accountbank, accountcreatortool, accountdeletortool, customersupport, deletesupportreport, stockeditor, banaccounts, unbanaccount
+from Data.Datas import produk, accounts, adminaccounts, superadminaccounts, bannedaccounts, historypesananlist, vouchers, adminchathistory, supporthistory, updatehistory
+getstates()
 loggedin = st.session_state.get("loggedin", False)
 adminloggedin = st.session_state.get("adminloggedin", False)
 superadminlogin = st.session_state.get("superadminlogin", False)
+banned = st.session_state.get("banned", False)
 dataaccess = st.session_state.get("databankaccess", False)
 
-if not loggedin and not adminloggedin and not superadminlogin:
+if not loggedin and not adminloggedin and not superadminlogin and not banned:
     halamanlogin = st.sidebar.radio(
         "Halaman",
-        ["Login", "Daftar"]
+        ["Login", "Daftar", "Updates"]
     )
     
     if halamanlogin == "Login":
@@ -24,8 +25,11 @@ if not loggedin and not adminloggedin and not superadminlogin:
             login(username, password)
     
     if halamanlogin == "Daftar":
-        st.title("Daftar")
-        st.warning("Fitur ini sedang In Development!")
+        register()
+    
+    if halamanlogin == "Updates":
+        st.title("Updates")
+        updatehistory()
 
 elif st.session_state.loggedin:
     st.sidebar.title(f"Hello {st.session_state.displayname}!")
@@ -53,13 +57,13 @@ elif st.session_state.loggedin:
         if st.button("Refresh"):
             st.rerun()
         if supporthistory:
-            st.dataframe(supporthistory, column_config={"Message": st.column_config.TextColumn(width="large")})
+            st.dataframe(supporthistory)
 
 elif st.session_state.adminloggedin:
     st.sidebar.title(f"Selamat Datang Admin {st.session_state.displayname}!")
     halamanadmin = st.sidebar.radio(
         "Halaman Admin",
-        ["Pesanan", "Vouchers", "Admin Chat", "Support Center", "Katalog", "Keranjang"]
+        ["Pesanan", "Vouchers", "Admin Chat", "Support Center", "Katalog", "Keranjang", "Updates"]
     )
 
     if st.sidebar.button("Logout"):
@@ -71,7 +75,7 @@ elif st.session_state.adminloggedin:
         pestab = st.tabs(["History Pesanan", "Delete Pesanan"])
         with pestab[0]:
             if historypesananlist:
-                st.data_editor(historypesananlist, num_rows="dynamic")
+                st.dataframe(historypesananlist)
                 downloadlist()
             else:
                 st.warning("Belum ada pesanan.")
@@ -112,12 +116,16 @@ elif st.session_state.adminloggedin:
     if halamanadmin == "Keranjang":
         st.title("Keranjang")
         keranjangfunc()
+    
+    if halamanadmin == "Updates":
+        st.title("Updates")
+        updatehistory()
 
 elif st.session_state.superadminlogin:
     st.sidebar.title(f"Selamat Datang Developer {st.session_state.displayname}!")
     halamanspadmin = st.sidebar.radio(
         "Halaman Developer",
-        ["Pesanan", "Vouchers", "Admin Chat", "Dev Tools", "Account Bank", "Support Center", "Katalog", "Keranjang"]
+        ["Pesanan", "Vouchers", "Stok Produk", "Admin Chat", "Dev Tools", "Account Bank", "Support Center", "Katalog", "Keranjang", "Updates"]
     )
 
     if st.sidebar.button("Logout"):
@@ -130,7 +138,7 @@ elif st.session_state.superadminlogin:
         pestab = st.tabs(["History Pesanan", "Delete Pesanan"])
         with pestab[0]:
             if historypesananlist:
-                st.data_editor(historypesananlist, num_rows="dynamic")
+                st.dataframe(historypesananlist)
                 downloadlist()
             else:
                 st.warning("Belum ada pesanan.")
@@ -144,12 +152,15 @@ elif st.session_state.superadminlogin:
             vouchermaker()
         with vtab[1]:
             voucherdeleter()
+    
+    if halamanspadmin == "Stok Produk":
+        stockeditor(nama="Kelepon")
 
     if halamanspadmin == "Admin Chat":
         st.title("Admin Chat")
         adminchat()
         if adminchathistory:
-            st.data_editor(adminchathistory, num_rows="dynamic", column_config={"Message": st.column_config.TextColumn(width="large")})
+            st.dataframe(adminchathistory, column_config={"Message": st.column_config.TextColumn(width="large")})
         else:
             st.warning("Belum ada chat.")
         
@@ -158,7 +169,7 @@ elif st.session_state.superadminlogin:
             st.rerun()
 
     if halamanspadmin == "Dev Tools":
-        tabs = st.tabs(["Account Creator", "Account Deleter"])
+        tabs = st.tabs(["Account Creator", "Account Deleter", "Ban Account", "Unban Account"])
         with tabs[0]:
             st.title("Account Creation Tool")
             selectedtype = st.selectbox("Create what Account Type", ["User Account", "Admin Account"])
@@ -173,6 +184,29 @@ elif st.session_state.superadminlogin:
             delselectedtype = st.selectbox("What Account Type you trying to delete", ["User Account", "Admin Account"])
             if st.button("Delete Account"):
                 accountdeletortool(deluserinput, delselectedtype)
+        with tabs[2]:
+            st.title("Account Ban")
+            banuserinput = st.text_input("Enter Account Username to Ban")
+            reason = st.text_input("Reason for Banning")
+            if st.button("Ban Account"):
+                banaccounts(banuserinput, reason)
+            
+            if bannedaccounts:
+                st.write("Banned Accounts")
+                st.dataframe(bannedaccounts)
+            else:
+                st.write("No banned accounts")
+        with tabs[3]:
+            st.title("Unban Accounts")
+            unbanuserinput = st.text_input("Enter Account Username to Unban")
+            if st.button("Unban Account"):
+                unbanaccount(unbanuserinput)
+            
+            if bannedaccounts:
+                st.write("Banned Accounts")
+                st.dataframe(bannedaccounts)
+            else:
+                st.write("No banned accounts")
 
     if halamanspadmin == "Account Bank":
         st.title("Account Databank")
@@ -209,3 +243,16 @@ elif st.session_state.superadminlogin:
     if halamanspadmin == "Keranjang":
         st.title("Keranjang")
         keranjangfunc()
+    
+    if halamanspadmin == "Updates":
+        st.title("Updates")
+        updatehistory()
+
+elif st.session_state.banned:
+    st.title("This Account has been Banned!")
+    st.write(f"Reason: {st.session_state.bannedreason}")
+    st.write("If you think this was an Accident, please contact an Admin/Dev.")
+
+    if st.sidebar.button("Logout"):
+        st.session_state["banned"] = False
+        st.rerun()
